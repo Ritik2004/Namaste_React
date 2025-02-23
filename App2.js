ecd25a342d527a11299648af96c7904d9a136389
Ritik2004 <hritikgaur44@gmail.com>
2025-02-23 12:27:11 +0530

    create react app

--- App2.js
+++ App2.js
@@ -2,6 +2,9 @@
 // heading is a Element it is a Object.Render convert this object to html.
 //Below we create parent element in which we are having nested elements, this structure can get complex for that we
 //use JSX. So react can be created like this but it gets complicated to write and read code like this.
+
+import React from "react";
+import ReactDOM from "react-dom/client"
 const parent = React.createElement('div',{id:'parent'},
     React.createElement("div",{id:"child"},[
         React.createElement("h1",{},"I am a h1 tag"),
